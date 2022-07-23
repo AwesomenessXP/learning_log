3b9d01293780b12a4c0aeb1cb25e5a09a03e515e
HaskMoney <heartstring85@gmail.com>
2022-07-22 17:03:52 -0700

    fixing UI

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Topic(models.Model):
     """A topic the user is learning about"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    # foreign key relationship to User model
+    owner = models.ForeignKey(User, on_delete=models.CASCADE) 
 
     def __str__(self): # returns a string of Topic.text
         """ Return a string representation of the model """
@@ -13,7 +17,7 @@ class Topic(models.Model):
 class Entry(models.Model):
     """Something specific learned about a topic"""
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    text = models.TextField(max_length=50)
+    text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
 # this class holds extra information about the model
@@ -27,6 +31,7 @@ class Entry(models.Model):
 
 class Pizzeria(models.Model):
     name = models.CharField(max_length=50)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.name[:50]}"
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 
 from .models import Topic, Pizzeria, Entry, Pizza
 from .forms import TopicForm, PizzeriaForm, EntryForm, PizzaForm
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 # Create your views here.
 # the render() funtion renders the response based on data provided by views
@@ -18,6 +20,8 @@ HOW TO MAKE A NEW PAGE:
 """ when URL request matches the pattern, Django looks for 'request' object 
     and also the html file
 """
+
+@login_required # apply this function to run it BEFORE the following function
 def index(request):
     """The home page for Learning Log"""
     # when called, returns contents of index.html
@@ -31,21 +35,26 @@ def silly(request):
     # when called, returns contents of silly.html
     return render(request, 'learning_logs/silly.html')
 
+@login_required # apply this function to run it BEFORE the following function
 def topics(request):
     """Show all topics"""
-
-    # query the database -> ask for Topic objects sorted by 'date_added'
-    topics = Topic.objects.order_by('date_added')
+    # Restrict topics to appropriate users:
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
 
     # create a dictionary that we'll use to ACCESS DATA
     context = {'topics':topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required # apply this function to run it BEFORE the following function
 def topic(request, topic_id):
     """Show a single topic and all its entries"""
 
     # we want to access topic_id attributes from the class
     topic = Topic.objects.get(id=topic_id) #ex: 1, 2, 3
+    
+    # Make sure the topic belongs to the current user
+    if topic.owner != request.user:
+        raise Http404 # return 404 page
 
     # REMEMBER: we can access the related objects bc of 'one to many' relationship
     entries = topic.entry_set.order_by('-date_added') #ex: 7/20/22, 7/21/22
@@ -54,17 +63,26 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required # apply this function to run it BEFORE the following function
 def pizza(request):
-    pizzerias = Pizzeria.objects.all() # send this data to the html page
+    # Restrict topics to appropriate users:
+    pizzerias = Pizzeria.objects.filter(owner=request.user).all()
     context = {'pizzerias': pizzerias}
     return render(request, 'learning_logs/pizzerias.html', context)
 
+@login_required # apply this function to run it BEFORE the following function
 def pizzas(request, pizzeria_id):
     pizzeria = Pizzeria.objects.get(pk=pizzeria_id) # the ID (some integer) of the page depends on the pizzeria id
+
+    # Make sure the topic belongs to the current user
+    if pizzeria.owner != request.user:
+        raise Http404 # return 404 page
+
     pizzas = pizzeria.pizza_set.all()
     context = {'pizzas': pizzas, 'pizzeria': pizzeria}
     return render(request, 'learning_logs/pizzas.html', context)
 
+@login_required # apply this function to run it BEFORE the following function
 def new_topic(request):
     """Add a new topic"""
     # IMPORTANT:
@@ -83,8 +101,11 @@ def new_topic(request):
 
     # if valid, redirect to topics, else -> display blank form
     if (form.is_valid()):
-        form.save()
 
+            # commit=False means make a new entry object without saving it to the database
+        new_topic = form.save(commit = False) # dont save in database YET
+        new_topic.owner = request.user # set owner to current user
+        new_topic.save() # save in database
         # home -> topic page -> click on 'add topic' -> new_topic page -> original topic page with new item
         return redirect('learning_logs:topics') # go back to page where topics are listed
 
@@ -92,6 +113,7 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required # apply this function to run it BEFORE the following function
 def new_pizzeria(request):
     if (request.method != 'POST'):
         form = PizzeriaForm() # make a blank form if nothing to send to server
@@ -99,14 +121,17 @@ def new_pizzeria(request):
         form = PizzeriaForm(data=request.POST)
     
     if (form.is_valid()):
-        form.save()
-
+        # commit=False means make a new entry object without saving it to the database
+        new_pizzeria = form.save(commit = False) # dont save in database YET
+        new_pizzeria.owner = request.user # set owner to current user
+        new_pizzeria.save() # save in database
         # home -> pizzeria page -> click on 'add pizzeria' -> new_pizzeria page -> original pizzeria page with new item
         return redirect('learning_logs:pizzerias') # go back to page where pizzerias are listed  
 
     context = {'form':form}  
     return render(request, 'learning_logs/new_pizzeria.html', context) 
 
+@login_required # apply this function to run it BEFORE the following function
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic"""
     topic = Topic.objects.get(id=topic_id)
@@ -119,16 +144,16 @@ def new_entry(request, topic_id):
     
         # return response based on request
         if (form.is_valid()):
-            # commit=False means make a new entry object without saving it to the database
-            new_entry = form.save(commit=False)
-            new_entry.topic = topic
-            new_entry.save()
+            new_topic = form.save(commit = False)
+            new_topic.topic = topic
+            new_topic.save() # save in database
             return redirect('learning_logs:topic', topic_id=topic_id)
 
     # Display a blank or invalid form
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html',context)
 
+@login_required # apply this function to run it BEFORE the following function
 def new_pizza(request, pizzeria_id):
     # get parent object
     pizzeria = Pizzeria.objects.get(id=pizzeria_id) # get id for pizza objects, will use them to return later
@@ -139,19 +164,25 @@ def new_pizza(request, pizzeria_id):
     else:
         form = PizzaForm(data=request.POST)
         if form.is_valid():
-            new_pizza = form.save(commit=False)
-            new_pizza.pizzeria = pizzeria
-            new_pizza.save()
+            # commit=False means make a new entry object without saving it to the database
+            new_topic = form.save(commit = False) # dont save in database YET
+            new_topic.pizzeria = pizzeria
+            new_topic.save() # save in database
             return redirect('learning_logs:pizza', pizzeria_id=pizzeria_id)
 
     context = {'pizzeria':pizzeria, 'form': form}
     return render(request, 'learning_logs/new_pizza.html', context)
 
+@login_required # apply this function to run it BEFORE the following function
 def edit_entry(request, entry_id):
     # when edit_entry recieves a GET request, edit_entry() returns a form for editing
     """Edit an existing entry"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+
+    # Make sure the topic belongs to the current user
+    if topic.owner != request.user:
+        raise Http404 # return 404 page
 
     if (request.method != 'POST'):
         # initial request: pre-fill form with current entry
@@ -170,6 +201,10 @@ def edit_entry(request, entry_id):
 def edit_pizza(request, pizza_id):
     pizza = Pizza.objects.get(id=pizza_id) # create a form instance based on existing object
     pizzeria = pizza.pizzeria # get the parent (we'll display as output)
+
+    # Make sure the topic belongs to the current user
+    if pizzeria.owner != request.user:
+        raise Http404 # return 404 page
 
     if (request.method != 'POST'):
         form = PizzaForm(instance=pizza)
